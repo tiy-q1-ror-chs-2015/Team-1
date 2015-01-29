6f1da7f0da81422582c160a45297b43a12a4f1b3
Matthew Gallo <mdgallo07@gmail.com>
2015-01-29 14:57:41 -0500

    Working on CRUD for schools controller.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,2 +1,34 @@
 class SchoolsController < ApplicationController
+  def index
+    @schools = School.all
+  end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    @school = School.create school_params
+    redirect_to schools_path
+  end
+
+  def show
+    @school = School.find params[:id]
+  end
+  def edit
+    @school = Medication.find params[:id]
+  end
+private
+  def school_params
+    params.require(:school).permit(
+      :application_deadline,
+      :average_sat,
+      :average_act,
+      :school_name,
+      :student_population,
+      :city,
+      :state,
+      :comment
+      )
+  end
 end
