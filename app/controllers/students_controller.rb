87bccc915da5fb135d9b142ac73b1171c1c96ffa
Ryan Mitchell <ryan@napaswdev.com>
2015-01-29 20:54:56 -0500

    Give Student #full_name. Add link in StudentsController to student_experiences_path.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,26 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :edit, :update, :destroy]
+  before_action :set_new_student, only: [:new, :create]
+  before_action :set_form, only: [:new, :create, :update, :edit]
 
   def index
     @students = Student.all
   end
 
+  def show
+    # before: set_student
+  end
+
+  def edit
+    # before: set_student and set_form
+  end
+
   def new
-    @form = StudentForm.new(Student.new)
+    # before: set_new_student and set_form
   end
 
   def create
-    @student = Student.new
-    @form = StudentForm.new(@student)
+    # before: set_new_student and set_form
     if @form.validate(params[:student])
       @form.save
       flash[:success] = 'Student saved'
@@ -22,11 +31,27 @@ class StudentsController < ApplicationController
     end
   end
 
-  def show
-
+  def update
+    # before: set_student and set_form
+    if @form.validate(params[:student])
+      @form.save
+      flash[:success] = 'Student updated'
+      redirect_to @student
+    else
+      flash[:error] = 'Could not save student'
+      render :edit
+    end
   end
 
   def destroy
+    # before: set_student
+    if @student.destroy
+      flash[:success] = 'Student deleted!'
+      redirect_to students_path
+    else
+      flash[:error] = 'Could not delete student'
+      render :show
+    end
   end
 
 
@@ -35,6 +60,14 @@ class StudentsController < ApplicationController
 
   def set_student
     @student = Student.find(params[:id])
+  end
+
+  def set_new_student
+    @student = Student.new
+  end
+
+  def set_form
+    @form = StudentForm.new(@student)
   end
 
 end
