ac30eef3ee6aa2ed2f5f84e366fdc49e9a6a21f1
Ryan Mitchell <ryan@napaswdev.com>
2015-01-30 13:31:32 -0500

    Setup basic abide stuff in helper and form component partial.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,5 +1,8 @@
 module FormHelper
-  def field form, field, type=:text_field, required: true, placeholder:nil, **args
-    render partial:'form_component', locals: {f: form, field: field, type:type, required: required, placeholder: placeholder, args:args }
+  def field form, field, type=:text_field, required: true, placeholder:nil, error:nil, **args
+    if required && error.nil?
+      error = "#{field.to_s.titleize.capitalize} is required."
+    end
+    render partial:'form_component', locals: {f: form, field: field, type:type, required: required, placeholder: placeholder, error: error, args:args }
   end
 end
