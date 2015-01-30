83f005459d33e61f74f3991043149e8f47787b39
Matthew Gallo <mdgallo07@gmail.com>
2015-01-29 19:16:14 -0500

    Starting Devise setup

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -13,12 +13,4 @@ class ExperiencesController < ApplicationController
 
   def destroy
   end
-
-  private
-  def exp_params
-    params.require(:test_scores)permit (
-      :title,
-      :description
-      )
-  end
 end
