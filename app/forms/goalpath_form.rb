31ea56da8d36b315ab3d0ffd481b4d10d9b40368
Ryan Mitchell <ryan@napaswdev.com>
2015-01-30 01:42:49 -0500

    Continue work on Goalpaths CRUD.

--- app/forms/goalpath_form.rb
+++ app/forms/goalpath_form.rb
@@ -1,5 +1,14 @@
 class GoalpathForm < Reform::Form
+
+  include Coercion
+
   property :end_goal, validates: { presence: true, length: { minimum: 5 } }
 
-  collection :schools
+  class ArrayOfSchools < Virtus::Attribute
+    def coerce(value)
+      School.find(value.reject(&:empty?))
+    end
+  end
+
+  property :schools, type: ArrayOfSchools
 end
