23d064fa2e2ec2de07f4b64a8950399a06d080ec
Ryan Mitchell <ryan@napaswdev.com>
2015-01-29 20:57:33 -0500

    Experiences CRUD, and basic views for most.

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,35 +1,63 @@
 class ExperiencesController < ApplicationController
   before_action :set_student
   before_action :set_experience, only: [:show, :edit, :update, :destroy]
+  before_action :set_new_experience, only: [:new, :create]
+  before_action :set_form, only: [:new, :create, :update, :edit]
+
+  # BEFORE ALL ACTIONS: set_student
 
   def index
     @experiences = @student.experiences
   end
 
+  def show
+    # before: set_experience
+  end
+
   def new
-    @experience = @student.experiences.new
-    @form = ExperienceForm.new(@experience)
+    # before: set_new_experience and set_form
   end
 
   def create
-    @experience = @student.experiences.new
-    @form = ExperienceForm.new(@experience)
+    # before: set_new_experience and set_form
     if @form.validate(params[:experience])
       @form.save
       flash[:success] = 'Experience saved.'
-      redirect_to @experience
+      redirect_to_experience
     else
       flash[:error] = 'Could not save experience.'
       render :new
     end
-  end  
+  end
 
-  def show
+  def edit
+    # before: set_experience and set_form
+  end
+
+  def update
+    # before: set_experience and set_form
+    if @form.validate(params[:experience])
+      @form.save
+      flash[:success] = 'Experience updated.'
+      redirect_to_experience
+    else
+      flash[:error] = 'Could not update experience.'
+      render :edit
+    end
   end
 
   def destroy
+    # before: set_experience
+    if @experience.destroy
+      flash[:success] = 'Experience deleted!'
+      redirect_to @student
+    else
+      flash[:error] = 'Could not delete experience'
+      render :show
+    end
   end
-<<<<<<< HEAD
+
+  private
 
   def set_student
     @student = Student.find params[:student_id]
@@ -38,6 +66,16 @@ class ExperiencesController < ApplicationController
   def set_experience
     @experience = Experience.find params[:id]
   end
-=======
->>>>>>> 83f005459d33e61f74f3991043149e8f47787b39
+
+  def set_new_experience
+    @experience = @student.experiences.new
+  end
+
+  def set_form
+    @form = ExperienceForm.new(@experience)
+  end
+
+  def redirect_to_experience
+    redirect_to [@student, @experience]
+  end
 end
