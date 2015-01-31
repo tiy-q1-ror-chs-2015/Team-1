872e1f00135fc614b2b6481554bc5cbdc25eabcd
Ryan Mitchell <ryan@napaswdev.com>
2015-01-31 11:07:53 -0500

    Some styling, fix registrations_controller, and fix routing for the same.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-      sessions: 'sessions'
+      sessions: 'sessions', registrations: 'registrations'
   }
   resources :students do
     resources :references
