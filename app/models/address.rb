c132d6ad463b5e420defcb40b1295de07da49cfb
Ryan Mitchell <ryan@napaswdev.com>
2015-02-01 02:16:04 -0500

    Convert over schools#index and schools#show.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,16 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
   belongs_to :state
+
+  def full_address array:false
+    if array
+      full_address_array
+    else
+      full_address_array.join('\n')
+    end
+  end
+
+  def full_address_array
+    [street, ("#{city}, #{state} #{zip}" if city), (state.country unless state.country == 'USA')].compact
+  end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,9 +1,18 @@
 class School < ActiveRecord::Base
+
+  include Addressable
+
   has_many :students, through: :student_schools
   has_many :student_schools, dependent: :destroy
 
   has_many :goalpaths, through: :goalpath_schools
   has_many :goalpath_schools, dependent: :destroy
 
-  has_one :address, as: :addressable
+  def average_sat_components
+    [[percentile_25_sat_verbal, percentile_25_sat_math].compact.inject(:+), [percentile_75_sat_verbal, percentile_75_sat_math].compact.inject(:+)]
+  end
+
+  def average_act_components
+    [percentile_25_act_composite, percentile_75_act_composite]
+  end
 end
