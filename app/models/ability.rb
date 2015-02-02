8eedb339ce7a1de20046ebc24a70604908f6fc3d
Ryan Mitchell <ryan@napaswdev.com>
2015-02-02 02:09:43 -0500

    Fix bug with Essay permissions.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -46,6 +46,7 @@ class Ability
         can [:index, :show, :create, :read, :update, :destroy], TestScore, student_id: id
         can [:index, :show, :create, :read, :update, :destroy], Experience, student_id: id
         can [:index, :show, :create, :read, :update, :destroy], Reference, student_id: id
+        can [:index, :show, :create, :read, :update, :destroy], Essay, student_id: id
         can [:search], School
       else
         can [:create], Student
